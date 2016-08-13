e90b8a4c9da3cee7e1e63dbd12819f41ddf58e55
Paul-Kijtapart <scc385@gmail.com>
2016-08-13 00:25:45 -0700

    add regex to check the input in form

--- php/DekDoi-form.php
+++ php/DekDoi-form.php
@@ -1,19 +1,106 @@
 <?php 
 
-// var_dump($_POST);
-// echo $_POST["name"];
-// echo $_POST["email"];
-// echo $_POST["phone"];
-// echo $_POST["comment"];
+/* Owner Info */
+$name = "Trirutda Nawattranakul";
+$email = "soda_zai@hotmail.com";
+$email = "soda_zai@hotmail.com";
+$topic = "DekDoi portfolio";
 
+/* Client info */
+$clientName = $clientEmail = $clientPhone = $clientMessage = "";
 
-$in = $_POST;
+/* Error Message */
+$nameError = $emailError = $phoneError = $messageError = "";
 
+/* Validate Client info in case Javascript suppoert is turned off */
+$flag = true; // Used to check if email is appropriate
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$clientName = checkName($_POST["name"]);
+	$clientEmail = checkEmail($_POST["email"]);
+	$clientPhone = checkPhone($_POST["phone"]);
+	$clientMessage = checkMessage($_POST["comment"]);
+}
 
-$myEmail = "soda_zai@hotmail.com";
-$thankYouEmail = $in["email"];
+/* Exit if inputs are not appropriate */
+if (!$flag) {
+	exit();
+}
 
+/* Check if name only contains letters and whitespaces */
+function checkName($value='')
+{
+	$value = init($value);
+	$pattern = '/^[A-Za-z\s]*$/';
+	if (!preg_match($pattern, $value)){
+		$nameError = "Name is not valid.";
+		echo $nameError;
+	}
+	return $value;
+}
 
+/* Check if the value is a valid email address */
+function checkEmail($value='')
+{
+	$value = init($value);
+	if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+		$emailError = "Email is not valid";
+		echo $emailError;
+	}
+	return $value;
+}
+
+/* Check if the value contains only the number */
+function checkPhone($value='')
+{
+	$value = init($value);
+	$pattern = '/(\+)?[\d]*\d{3}([\.\-\:])?\d{3}\2\d{4}/';
+	if (!preg_match($pattern, $value)) {
+		$phoneError = "Phone is not valid.";
+		echo $phoneError;
+	}
+	return $value;
+}
+
+function checkMessage($value='')
+{
+	$value = init($value);
+	// $pattern = '[^a-zA-Z0-9\s\-\.\,\:\;\_\?\*\$\(\)]';
+	// if (preg_match($pattern, $value)) {
+	// 	$messageError = "Message contains inappropriate data. Please try again.";
+	// 	echo $messageError;
+	// }
+	return $value;
+}
+
+function init($data) {
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+/* Printing */
+var_dump($clientName);
+var_dump($clientEmail);
+var_dump($clientPhone);
+var_dump($clientMessage);
+
+/* Send Thank you Email 
+Each line should be separated with a CRLF (\r\n). 
+Lines should not be larger than 70 characters.
+*/
+$thankyouEmail = 'Dear ' . $clientName . "\r\n";
+$thankyouEmail .= 'Thank you for taking time to contact me.' . "\r\n";
+$thankyouEmail .= 'Kind regards,' . "\r\n";
+$thankyouEmail .= $name . "\r\n"; 
+
+/* Send Email to Owner */
+$emailToOwner = $topic . "\r\n";
+$emailToOwner .= $clientName . " would like to contact you about " . $clientMessage . ".\r\n";
+$emailToOwner .= "His phone number is " . $clientPhone . " and his email is " . $clientEmail . "\r\n";
+
+/* Send Emails */
+//$thankyouStatus = mail($clientEmail, $topic, $thankyouEmail);
+//$ownerStatus = mail($email, $topic, $emailToOwner);
 
 
  ?>
--- php/DekDoi.php
+++ php/DekDoi.php
@@ -1,16 +1,5 @@
 <?php
-// $data = $_POST;
-// $email = "soda_zai@hotmail.com"
-// $topic = "Oil portfolio";
-// $clientName = $data["name"];
-// $clientEmail = $data["email"];
-// $clientPhone = $data["phone"];
-// $clientMessage = $data["message"];
 
-var_dump($_POST["name"]);
-var_dump($_POST["email"]);
-var_dump($_POST["phone"]);
-var_dump($_POST["comment"]);
 
 
 
